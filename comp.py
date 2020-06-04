d9433f48468597ef1f513404a3ea6db297064f18
DegenerateTrash <blakey983@gmail.com>
2020-06-04 17:05:57 +0100

    Fixed comp.py and allowed the user to specify a file

--- comp.py
+++ comp.py
@@ -1,15 +1,15 @@
 from PIL import Image
 
 def main():
-    im = Image.open("head.png")
+    im = Image.open(str(input("Image Name: ")))
     print(im.height+(im.width*im.height))
     im.convert('L').save("gr.png")
     im = Image.open("gr.png")
     out = im
     out
     arr = []
-    for x in range(im.width):
-        for y in range(im.height):
+    for y in range(im.height):
+        for x in range(im.width):
             if out.getpixel((x,y)) <= 30:
                 out.putpixel((x,y),255)
                 arr.append("1")
@@ -29,35 +29,39 @@ def strToimg():
     #print(arr)
     y=0
     x=0
-    for f in arr:
-        if f == "\n":
+    for f in range(0,len(arr)-1,1):
+        if arr[f] == "\n":
             y+=1
-        if f != "\n" and y == 0:
+        if arr[f] != "\n" and y == 0:
             x+=1
     print(y)
     print(x)
     #ou = Image.open("blnk.jpg").convert('L')
-    ou = Image.new("L", (1193,y),color = "white")
+    ou = Image.new("L", (x,y),color = "white")
     y = 0
     x = 0
     narr = []
     #for f in range(0,len(arr)-1,1):
-    for f in arr.split(" "):
+    for f in range(0,len(arr)-1,1):
+        #print(arr[f])
         try:
-            if f == "\n":
+            
+            if arr[f] == "\n":
                 y += 1
                 x = 0
-            if f == "1":
+            if arr[f] == "1":
                 ou.putpixel((x,y),255)
-            if f == "0":
+                #print("1")
+            if arr[f] == "0":
                 ou.putpixel((x,y),0)
             x += 1
         except ValueError:
             i = 0
         #print(int(arr[x]))
     #print(y)
-    ou = ou.rotate(-90)
+    #ou = ou.rotate(-90)
     ou.save("Result.png")
+    ou.show()
     #print(str("".join(narr)))
     
     return
